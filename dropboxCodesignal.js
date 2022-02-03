fc8d58e0bd0c65356d77f730ff7b6cea2b2a1442
Rob Strasser <roestrasser@sbcglobal.net>
2022-02-02 16:31:02 -0800

    problems

--- dropboxCodesignal.js
+++ dropboxCodesignal.js
@@ -17,4 +17,51 @@ function solution(numbers) {
     return sol
 }
 
-console.log(solution([1, 2, 1, 3, 4])) // [1, 1, 0]
+// console.log(solution([1, 2, 1, 3, 4])) // [1, 1, 0]
+
+function stringSol(string) {
+    if (string.length < 4) return 0;
+
+    let count = 0;
+
+    for(let i = 0; i < string.length; i++) {
+
+    }
+
+    return count;
+}
+
+// console.log(stringSol('xzxzx')) // 5
+
+// construct a string from words in arr
+function strArr(arr) {
+    let str = '';
+
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+    return str
+}
+
+// console.log(strArr(['Daisy', 'Rose', 'Hyacinth', 'Poppy'])) // 'DRHPaoyoisapsecpyiynth'
+
+
+// contiguous subarrays
+function subArr(arr) {
+    if (arr === []) return 0;
+
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        const ele = arr[i];
+
+        let j = arr[i + 1];
+        while (j < arr[j - 1]) {
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(subArr([9, 8, 7, 6, 5])) // 15
+console.log(subArr([10, 10, 10])) // 3
