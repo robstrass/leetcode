f1a7ece8878716e652ec0cab5467c1ce95e6151b
Rob Strasser <roestrasser@sbcglobal.net>
2022-02-14 15:16:35 -0800

    maximum subarray

--- problems.js
+++ problems.js
@@ -130,3 +130,14 @@ var productExceptSelf = function(nums) {
 
     return sol;
 };
+
+// Maximum subarray
+var maxSubArray = function(nums) {
+    let sum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+        sum = Math.max(sum, nums[i]);
+    }
+
+    return sum;
+};
