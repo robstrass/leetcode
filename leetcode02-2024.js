77ae021509d6f2d254798d7fb8aa618a92c4daf3
Rob Strasser <robstrasserdev@gmail.com>
2024-02-29 17:30:05 -0800

    finished lc 605

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -56,3 +56,25 @@ var kidsWithCandies = function (candies, extraCandies) {
 
   return result;
 };
+
+// 605. Can Place Flowers
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  let count = 0;
+
+  for (let i = 0; i < flowerbed.length; i += 2) {
+    if (flowerbed[i] === 0) {
+      if (i === flowerbed.length - 1 || flowerbed[i + 1] === 0) {
+        count++;
+      } else {
+        i++;
+      }
+    }
+  }
+
+  return count >= n;
+};
