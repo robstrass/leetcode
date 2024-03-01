d4d3ff4b69aeb62001994403c45e5f0eb6a22c07
Rob Strasser <robstrasserdev@gmail.com>
2024-03-01 14:20:39 -0800

    finished lc 238

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -155,3 +155,26 @@ var reverseWords = function (s) {
 
   return res.join(" ");
 };
+
+// 238. Product of Array Except Self
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const res = [];
+  let left = 1;
+  let right = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    res[i] = left;
+    left *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    res[i] *= right;
+    right *= nums[i];
+  }
+
+  return res;
+};
