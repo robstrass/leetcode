745c612343f2328140c3ddd7a4603899ed912f7e
Rob Strasser <robstrasserdev@gmail.com>
2024-03-02 19:41:11 -0800

    finished lc 283

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -136,6 +136,42 @@ var reverseVowels = function (s) {
   return arr.join("");
 };
 
+// 283. Move Zeroes
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let low = 0;
+  let high = 1;
+
+  while (high <= nums.length - 1) {
+    if (nums[low] !== 0) {
+      low++;
+      high++;
+    } else {
+      if (nums[high] !== 0) {
+        [nums[low], nums[high]] = [nums[high], nums[low]];
+        low++;
+      }
+      high++;
+    }
+  }
+};
+
+/*
+
+
+
+
+
+
+
+
+
+
+*/
+
 // MEDIUM
 
 // 151. Reverse Words in a String
