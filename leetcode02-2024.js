1472233f164ac737a2369748a4a88ad6c1d818bb
Rob Strasser <robstrasserdev@gmail.com>
2024-03-02 20:11:02 -0800

    finished lc 392

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -159,6 +159,28 @@ var moveZeroes = function (nums) {
   }
 };
 
+// 392. Is Subsequence
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  if (s.length === 0) return true;
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return i === s.length;
+};
+
 /*
 
 
