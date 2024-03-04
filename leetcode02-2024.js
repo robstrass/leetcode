ed698f8502181bd30ed0e86db1a28c764f2e47c3
Rob Strasser <robstrasserdev@gmail.com>
2024-03-04 12:46:12 -0800

    finished lc 643

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -181,6 +181,27 @@ var isSubsequence = function (s, t) {
   return i === s.length;
 };
 
+// 643. Maximum Average Subarray I
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  max = sum;
+  for (let i = k; i < nums.length; i++) {
+    sum += nums[i] - nums[i - k];
+    max = Math.max(max, sum);
+  }
+
+  return max / k;
+};
+
 /*
 
 
