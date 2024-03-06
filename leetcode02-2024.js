1b932b1ce8798619f4917a8d03d515cd5fab03fb
Rob Strasser <robstrasserdev@gmail.com>
2024-03-06 15:30:51 -0800

    finished lc 2215

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -238,6 +238,28 @@ var pivotIndex = function (nums) {
   return -1;
 };
 
+// 2215. Find the Difference of Two Arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function (nums1, nums2) {
+  const ans1 = [];
+  const ans2 = [];
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+
+  set1.forEach(num => {
+    if (!set2.has(num)) ans1.push(num);
+  });
+  set2.forEach(num => {
+    if (!set1.has(num)) ans2.push(num);
+  });
+
+  return [ans1, ans2];
+};
+
 /*
 
 
