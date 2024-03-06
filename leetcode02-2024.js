f1d200d5e35c0d7537219521b8730a46b3adceb2
Rob Strasser <robstrasserdev@gmail.com>
2024-03-05 18:32:32 -0800

    finished lc 724

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -219,6 +219,25 @@ var largestAltitude = function (gain) {
   return max;
 };
 
+// 724. Find Pivot Index
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let total = 0;
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) total += nums[i];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === total - nums[i] - leftSum) return i;
+    leftSum += nums[i];
+  }
+
+  return -1;
+};
+
 /*
 
 
