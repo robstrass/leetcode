ad047b81865b34015026df84e49f97616d9f26c8
Rob Strasser <robstrasserdev@gmail.com>
2024-03-08 14:49:55 -0800

    finished lc 2390

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -585,3 +585,21 @@ var equalPairs = function (grid) {
 
   return count;
 };
+
+// 2390. Removing Stars From a String
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "*") {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+
+  return stack.join("");
+};
