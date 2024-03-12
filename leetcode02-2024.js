3a4eb78939a9d72d35363d150afa89d95ad52c03
Rob Strasser <robstrasserdev@gmail.com>
2024-03-12 14:21:22 -0700

    finished lc 206

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -313,6 +313,32 @@ RecentCounter.prototype.ping = function (t) {
  * var param_1 = obj.ping(t)
  */
 
+// 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+
+  while (curr !== null) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+
 /*
 
 
