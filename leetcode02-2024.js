01a3a2e3cf30848d9570eddc30c21f55f9d74725
Rob Strasser <robstrasserdev@gmail.com>
2024-03-11 19:14:45 -0700

    finished lc 328

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -751,3 +751,43 @@ var deleteMiddle = function (head) {
   prev.next = slow.next;
   return head;
 };
+
+// 328. Odd Even Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (head === null || head.next === null) return head;
+
+  let odd = new ListNode(0);
+  let oddPtr = odd;
+  let even = new ListNode(0);
+  let evenPtr = even;
+  let idx = 1;
+
+  while (head !== null) {
+    if (idx % 2 === 0) {
+      evenPtr.next = head;
+      evenPtr = evenPtr.next;
+    } else {
+      oddPtr.next = head;
+      oddPtr = oddPtr.next;
+    }
+
+    head = head.next;
+    idx++;
+  }
+
+  evenPtr.next = null;
+  oddPtr.next = even.next;
+
+  return odd.next;
+};
