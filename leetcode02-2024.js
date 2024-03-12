11d7ccdfbc29c33d6dac5bb4a0f526c1d90f5bda
Rob Strasser <robstrasserdev@gmail.com>
2024-03-11 18:36:06 -0700

    finished lc 2095

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -722,3 +722,30 @@ var predictPartyVictory = function (senate) {
 
   return r.length ? "Radiant" : "Dire";
 };
+
+// 2095. Delete the Middle Node of a Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function (head) {
+  let slow = head;
+  let fast = head;
+  let prev;
+
+  while (fast !== null && fast.next !== null) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  prev.next = slow.next;
+  return head;
+};
