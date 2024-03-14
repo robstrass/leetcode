3b866de0fd429f50fad0793bb7251e70f50c42e1
Rob Strasser <robstrasserdev@gmail.com>
2024-03-13 19:18:23 -0700

    finished lc 746

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -355,6 +355,19 @@ var tribonacci = function (n, hash = {}) {
   return hash[n];
 };
 
+// 746. Min Cost Climbing Stairs
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  for (let i = cost.length - 3; i >= 0; i--) {
+      cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+  }
+
+  return Math.min(cost[0], cost[1]);
+};
+
 /*
 
 
