c063730e7047671e3ecb16ccf3cf594840485d2e
Rob Strasser <robstrasserdev@gmail.com>
2024-03-16 17:34:43 -0700

    finished lc 872

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -386,6 +386,39 @@ var maxDepth = function (root) {
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
 
+// 872. Leaf-Similar Trees
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function (root1, root2) {
+  function findLeaves(root, leaves) {
+    if (!root) return;
+    if (!root.left && !root.right) {
+      leaves.push(root.val);
+    }
+
+    findLeaves(root.left, leaves);
+    findLeaves(root.right, leaves);
+  }
+
+  let leaves1 = [];
+  let leaves2 = [];
+  findLeaves(root1, leaves1);
+  findLeaves(root2, leaves2);
+
+  return leaves1.toString() === leaves2.toString();
+};
+
 /*
 
 
