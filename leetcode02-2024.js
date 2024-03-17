6df9b03986ebfe8ef3eaa343505d684e32e65f0d
Rob Strasser <robstrasserdev@gmail.com>
2024-03-16 18:18:16 -0700

    finished lc 437

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -1167,3 +1167,43 @@ var goodNodes = function (root, max = -Infinity) {
 
   return res;
 };
+
+// 437. Path Sum III
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function (root, targetSum) {
+  if (!root) return 0;
+  let count = 0;
+
+  function helper(node, sum) {
+    if (!node) return;
+
+    sum += node.val;
+    if (sum === targetSum) count++;
+
+    helper(node.left, sum);
+    helper(node.right, sum);
+  }
+
+  function dfs(node) {
+    if (!node) return;
+
+    helper(node, 0);
+    dfs(node.left);
+    dfs(node.right);
+  }
+
+  dfs(root);
+  return count;
+};
