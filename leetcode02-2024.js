e96391c714aecebbf977d6c669d6e6411ba3c736
Rob Strasser <robstrasserdev@gmail.com>
2024-03-16 17:52:15 -0700

    finished lc 1448

--- leetcode02-2024.js
+++ leetcode02-2024.js
@@ -1138,3 +1138,32 @@ var minDistance = function (word1, word2) {
 
   return dp[m][n];
 };
+
+// 1448. Count Good Nodes in Binary Tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root, max = -Infinity) {
+  if (!root) return 0;
+
+  let res = 0;
+
+  if (root.val >= max) {
+    res++;
+    max = root.val;
+  }
+
+  res += goodNodes(root.left, max);
+  res += goodNodes(root.right, max);
+
+  return res;
+};
